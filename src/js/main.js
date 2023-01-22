dd5318841b58a0589eb533d1180ff4c3925e72a4
Foster <72973015+Foster0123@users.noreply.github.com>
2023-01-22 17:43:50 +0530

    Initial Commit Theme-Menu 2

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,14 @@
-console.log("Working")
+const theme = document.getElementsByClassName("theme")
+const currentTheme = document.getElementById("current-theme")
+const themes = document.getElementById("themes-section")
+
+currentTheme.addEventListener("click", () => {
+	themes.classList.toggle("display")
+})
+
+for (let i = 0; i < theme.length; i++) {
+	theme[i].addEventListener('click', (e) => {
+		currentTheme.getElementsByTagName("i")[0].className = 
+			e.currentTarget.getElementsByTagName('i')[0].className;
+	})
+}
